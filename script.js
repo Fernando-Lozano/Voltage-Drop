ffb32686127f8a3f1e99cf5b39fd86e4d3b03ba4
Fernando Lozano <fernando.k.lozano@gmail.com>
2020-08-27 23:28:40 -0500

    worked on main squares

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // closes navbar when link is clicked
 $('.navbar-nav>a').on('click', function () {
   $('.navbar-collapse').collapse('hide');
+  burger.classList.remove("open");
+  open = false;
 });
+// adds a class to toggler for animations
 const burger = document.querySelector(".navbar-toggler");
 var open = false;
 burger.addEventListener("click", () => {
