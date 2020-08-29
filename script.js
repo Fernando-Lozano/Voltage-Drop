48b036f47b19282983ecdfc918af72c9da4eeca1
Fernando Lozano <fernando.k.lozano@gmail.com>
2020-08-28 23:09:31 -0500

    expirementing

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // closes navbar when link is clicked
-$('.navbar-nav>a').on('click', function () {
-  $('.navbar-collapse').collapse('hide');
+$(".navbar-nav>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
   burger.classList.remove("open");
   open = false;
 });
 // adds a class to toggler for animations
 const burger = document.querySelector(".navbar-toggler");
-var open = false;
-burger.addEventListener("click", () => {
-  if (!open) {
+var firstOpen = false;
+burger.addEventListener("click", function () {
+  if (!firstOpen) {
     burger.classList.add("open");
-    open = true;
-  }
-  else {
+    firstOpen = true;
+  } else if (burger.classList.contains("collapsed")) {
+    burger.classList.add("open");
+  } else {
     burger.classList.remove("open");
-    open = false;
   }
 });
