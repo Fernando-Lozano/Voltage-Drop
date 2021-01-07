8459e42f25a5a1f717c1c7ae98dded0cf299c87b
Fernando <fernando.k.lozano@gmail.com>
2021-01-06 19:16:50 -0600

    add lightning effect

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const iconBars = document.querySelectorAll(".icon-bar");
 // closes navbar when link is clicked
 $(".navbar-nav>a").on("click", function () {
   $(".navbar-collapse").collapse("hide");
   burger.classList.remove("open");
   mainNav.classList.remove("initialNav");
+  for (bar of iconBars) {
+    bar.classList.toggle("flicker");
+  }
   open = false;
 });
 // adds a class to toggler for animations
@@ -13,15 +17,24 @@ var firstOpen = false;
 burger.addEventListener("click", function () {
   if (!firstOpen) {
     burger.classList.add("open");
+    for (bar of iconBars) {
+      bar.classList.toggle("flicker");
+    }
     firstOpen = true;
     mainNav.classList.add("initialNav");
     
   } else if (burger.classList.contains("collapsed") && !(collapsing.classList.contains("collapsing"))) {
     burger.classList.add("open");
+    for (bar of iconBars) {
+      bar.classList.toggle("flicker");
+    }
     mainNav.classList.add("initialNav");
 
   } else if (!(collapsing.classList.contains("collapsing"))) {
     burger.classList.remove("open");
+    for (bar of iconBars) {
+      bar.classList.toggle("flicker");
+    }
     mainNav.classList.remove("initialNav");
 
   }
